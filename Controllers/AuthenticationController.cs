7b56221a70fe220732ff3d1e5c6b23eff75a3030
Cristian Peralta <cristianeph@me.com>
2021-01-06 21:47:04 -0500

    Asegurando APIs con JWT, Controladores de autenticacion y clientes

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RetoBackendApp.Models;
+using RetoBackendApp.Services;
+
+namespace RetoBackendApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private IAuthenticationService _authenticationService;
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] User model)
+        {
+            var user = _authenticationService.Authenticate(model.UserName, model.Password);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Credenciales incorrectas" });
+            }
+            else
+            {
+                return  Ok(user);
+            }
+        }
+    }
+}
